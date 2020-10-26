8c7e33a00b93cdbc1e376ecd8eb2a8e841b26466
Jesus Valido Zafra <jesuszafra22@gmail.com>
2020-10-26 13:21:26 +0100

    Primeros dos módulos de Python

---
+++ clase_26_10_2020/Prueba_1.py
@@ -0,0 +1,9 @@
+'''
+Created on 26-oct-2020
+
+@author: maxus
+'''
+
+x = 1 + 2**3/4*5
+
+print(x)
---
+++ clase_26_10_2020/Yardas_Metros.py
@@ -0,0 +1,9 @@
+'''
+Created on 26-oct-2020
+
+@author: maxus
+'''
+
+yardas=input("Introduzca las yardas: ")
+
+print("\n",yardas," yardas son equivalentes a ", (int)(yardas)*0.9144," metros.")
